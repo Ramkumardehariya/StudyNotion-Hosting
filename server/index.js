0d3ff95ad11b16fbf5a19be862c1d14ec31919ff
Ramkumardehariya <ramkumardehariya26@gmail.com>
2026-03-31 20:59:47 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "*",
+		origin: "https://study-notion-frontend-seven-chi.vercel.app/",
 		credentials: true,
 	})
 );
